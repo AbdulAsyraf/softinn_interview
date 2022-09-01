f948e0132f50bd5c36ef2701ba2d30086d47578b
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-01 23:29:16 +0800

    trying out maps

--- q2.cpp
+++ q2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -9,18 +10,42 @@ void identify(string input_string, int length_input_string){
     vector<int> count_vowels, count_consonant, count_digits, count_others;
 }
 
-int main(){
-    
+void categorize(){
     string input_string;
     int length_input_string;
+    map<char, int> vowels, consonants, others;
 
     cout << "Please enter a string:\t";
 
-    cin >> input_string;
-
+    // cin >> input_string;
+    getline(cin, input_string);
+    
     length_input_string = input_string.length();
 
-    identify(input_string, length_input_string);
+    for (int i = 0; i < length_input_string; i++){
+        if (isalpha(input_string[i])){
+            cout << input_string[i] << " is an alphanumeric character" << endl;
+            if(input_string[i] == 'a')
+        }else{
+            cout << input_string[i] << " is not an alphanumeric character" << endl; 
+        }
+        
+    }
+}
+
+int main(){
+    
+    // string input_string;
+    // int length_input_string;
+
+    // cout << "Please enter a string:\t";
+
+    // cin >> input_string;
+
+    // length_input_string = input_string.length();
+
+    // identify(input_string, length_input_string);
+    categorize();
 
     return 0;
 }
