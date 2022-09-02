997dc69bf9dee539d6a7fb750fc80bc33acefa7f
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-02 17:38:06 +0800

    map implementation done

--- q2.cpp
+++ q2.cpp
@@ -11,9 +11,10 @@ void identify(string input_string, int length_input_string){
 }
 
 void categorize(){
-    string input_string;
-    int length_input_string;
-    map<char, int> vowels, consonants, others;
+    string input_string, longest_string = "", longest_word = "";
+    int length_input_string, longest = 0, longest_word_length = 0;
+    char current_char;
+    map<char, int> vowels, consonants, numbers, others;
 
     cout << "Please enter a string:\t";
 
@@ -23,18 +24,105 @@ void categorize(){
     length_input_string = input_string.length();
 
     for (int i = 0; i < length_input_string; i++){
-        if (isalpha(input_string[i])){
-            cout << input_string[i] << " is an alphanumeric character" << endl;
-            if(input_string[i] == 'a')
-        }else{
-            cout << input_string[i] << " is not an alphanumeric character" << endl; 
-        }
-        
+        if (isalnum(input_string[i])){
+            current_char = tolower(input_string[i]);
+            if(current_char == 'a' || current_char == 'e' || current_char == 'i' || current_char == 'o' || current_char == 'u'){
+                if(vowels.insert(pair<char, int>(current_char, 1)).second){
+                    ++longest;
+                    longest_string += current_char;
+                }else{
+                    vowels.at(current_char) = vowels.at(current_char) + 1;
+                    ++longest;
+                    longest_string += current_char;
+                }
+            }else if (isdigit(current_char)){
+                if(numbers.insert(pair<char, int>(current_char, 1)).second){
+                    ++longest;
+                    longest_string += current_char;
+                }else{
+                    numbers.at(current_char) = numbers.at(current_char) + 1;
+                    ++longest;
+                    longest_string += current_char;
+                }
+            }else{
+                if(consonants.insert(pair<char, int>(current_char, 1)).second){
+                    ++longest;
+                    longest_string += current_char;
+                }else{
+                    consonants.at(current_char) = consonants.at(current_char) + 1;
+                    ++longest;
+                    longest_string += current_char;
+                }
+            }  
+        }else{ 
+            if(others.insert(pair<char, int>(input_string[i], 1)).second){
+                if(longest > longest_word_length){
+                    longest_word = longest_string;
+                    longest_word_length = longest;
+                }
+                longest_string = "";
+                longest = 0;
+            }else{
+                others.at(input_string[i]) = others.at(input_string[i]) + 1;
+                if(longest > longest_word_length){
+                    longest_word = longest_string;
+                    longest_word_length = longest;
+                }
+                longest_string = "";
+                longest = 0;
+            }
+        } 
     }
+    
+    if(longest > longest_word_length){
+        longest_word = longest_string;
+        longest_word_length = longest;
+    }
+    longest_string = "";
+    longest = 0;
+
+    if (!vowels.empty()){
+        cout << "Vowels:\t\t";
+        for (auto& x: vowels){
+            cout << "(" << char(toupper(x.first)) << ") " << x.second << "   ";
+        }
+        cout << endl;
+    }
+
+    if(!consonants.empty()){
+        cout << "Consonants:\t";
+        for (auto& x: consonants){
+            cout << "(" << char(toupper(x.first)) << ") " << x.second << "   ";
+        }
+        cout << endl;
+    }
+
+    if(!numbers.empty()){
+        cout << "Numbers:\t";
+        for (auto& x: numbers){
+            cout << "(" << char(toupper(x.first)) << ") " << x.second << "   ";
+        }
+        cout << endl;
+    }
+
+    if(!others.empty()){
+        cout << "Symbols:\t";
+        for (auto& x: others){
+            if(x.first == ' '){
+                cout << "(Blank Space) " << x.second << "   ";
+            }else{
+                cout << "(" << char(toupper(x.first)) << ") " << x.second << "   ";
+            }
+        }
+        cout << endl;
+    }
+
+    cout << "\nThe longest word is \"" << longest_word << "\" with a length of " << longest_word_length << " characters"<< endl;
 }
 
 int main(){
-    
+
+
     // string input_string;
     // int length_input_string;
 
