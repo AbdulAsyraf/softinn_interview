ad6292c75fed62b6175e65fe42e2faf31a88b711
AbdulAsyraf <abdul.asyrafah@gmail.com>
2022-09-04 17:15:34 +0800

    done with shapes

--- q3.cpp
+++ q3.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main(){
-    int length, temp_y;
-    char shape;
-    length = 9;
+    int length = 10, temp_y, radius;
+    char shape = 'g';
 
-    shape = 'd';
+    cout << "Please choose a shape\nS - Square\tT - Triangle\tD - Diamond\tC - Circle\n";
+
+    while (!(tolower(shape) == 's' || tolower(shape) == 't' || tolower(shape) == 'd' || tolower(shape) == 'c')){
+        cout << ">";
+        cin >> shape;
+    }
+
+    cout << "Please enter a size\n>";
+    cin >> length;
 
     if (shape == 's'){
     //  square function
         for (int y = length/2; y >= -(length/2); y--){
             for (int x = -(length/2); x <= length/2; x++){
-                //cout << "*";
-                cout << x;
+                cout << "* ";
+                // cout << x;
             }
             cout << endl;
         }
@@ -24,28 +32,23 @@ int main(){
             temp_y = length - y;
             for (int x = -length; x <= length; x++){
                 if (x >= (-temp_y) && x <= temp_y)
-                    cout << "*";
+                    cout << "* ";
                 else
-                    cout << " ";
+                    cout << "  ";
             }
             cout << endl;
         }
     }else if (shape == 'd'){
     //  diamond function
         length = length/2;
-        // if (length%2){
-        //     length = length/2;
-        // }else{
-        //     length = length/2 + 1;
-        // }
 
         for (int y = length; y > 0; y--){
             temp_y = length - y;
             for (int x = -length; x <= length; x++){
                 if (x >= (-temp_y) && x <= temp_y)
-                    cout << "*";
+                    cout << "* ";
                 else
-                    cout << " ";
+                    cout << "  ";
             }
             cout << endl;
         }
@@ -53,9 +56,23 @@ int main(){
             temp_y = y + length;
             for (int x = -length; x <= length; x++){
                 if (x >= (-temp_y) && x <= temp_y){
-                    cout << "*";
+                    cout << "* ";
                 }else{
-                    cout << " ";
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
+    }else if (shape == 'c'){
+    //  circle function
+        radius = int(length/2);
+        for (int y = radius; y >= -radius; y--){
+            for (int x = -radius; x <= radius; x++){
+                // cout << x;
+                if (pow(x, 2) <= (pow(radius, 2) - pow(y, 2))){
+                    cout << "* ";
+                }else{
+                    cout << "  ";
                 }
             }
             cout << endl;
